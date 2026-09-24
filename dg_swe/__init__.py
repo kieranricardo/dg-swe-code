3d82eddc2fb82e28765b0feaf62bfa74b74fee21
Kieran Ricardo <kieran.ricardo@anu.edu.au>
2026-09-24 12:10:43 +1000

    add tswe solver

--- dg_swe/__init__.py
+++ dg_swe/__init__.py
@@ -7,3 +7,12 @@ __all__ = [
     "DGCubedSphereFaceTSWE",
     "DGCubedSphereTSWE",
 ]
+from dg_swe.dg_cubed_sphere_swe import DGCubedSphereFace, DGCubedSphereSWE
+from dg_swe.dg_cubed_sphere_tswe import DGCubedSphereFaceTSWE, DGCubedSphereTSWE
+
+__all__ = [
+    "DGCubedSphereFace",
+    "DGCubedSphereSWE",
+    "DGCubedSphereFaceTSWE",
+    "DGCubedSphereTSWE",
+]
